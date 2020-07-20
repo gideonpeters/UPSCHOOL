ff655e0a470b2c3cfeedb00fbc629be855a90d52
Gideon Peters <gideonpeters@Gideons-MacBook-Pro.local>
2020-07-20 20:52:40 +0100

    chore: production fix for storage

--- upschool-backend/config/filesystems.php
+++ upschool-backend/config/filesystems.php
@@ -79,8 +79,8 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
-        public_path('storage/images') => storage_path('app/images'),
-        public_path('storage/documents') => storage_path('app/documents'),
+        public_path('storage/images') => storage_path('app/public/images'),
+        public_path('storage/documents') => storage_path('app/public/documents'),
     ],
 
 ];
